eb9d582aeac84117ade479fac797e53316c29a6e
RickBarretto <pdantasbarreto@protonmail.com>
2021-02-12 16:35:09 -0300

    fixing some removed files

--- app.py
+++
@@ -1,3 +0,0 @@
-class App:
-    def run():
-        pass
--- help.py
+++
@@ -1,7 +0,0 @@
-class Help:
-    def run():
-        with open("help.txt", "r") as f:
-            h = f.read()
-            print(h)
-
-Help.run()
