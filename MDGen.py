122aa50ec8fe38c71bbb7d93939cd600873f848a
RickBarretto <pdantasbarreto@protonmail.com>
2021-02-12 15:30:12 -0300

    starting my console application, for MD Generator

---
+++ MDGen.py
@@ -0,0 +1,25 @@
+#Python Code
+
+""" What I want?
++ Name
++ Description
++ Functions, to codeblocks, h1 - h6, quotes and lists.
+"""
+
+class Terminal:
+    def run():
+        print("Welcome to Mardown - README - generator.\nWhat do you want?\nA) Help\tB) Create")
+        input1 = input(">> ")
+        if input1 == "a" or input1 == "A":
+            Terminal.run_help()
+        elif input1 == "b" or input1 == "B":
+            Terminal.run_app()
+        else:
+            print("[!] Choose the options beetwen A or B!")
+            Terminal.run()
+
+    def run_help():
+        pass
+    def run_app():
+        pass
+        
